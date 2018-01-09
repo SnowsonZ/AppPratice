a44e70dd7f7291f7df71761e421d31bd5578d298
Snowson <snowsonz@163.com>
2018-01-09 10:59:08 +0800

    沉浸式状态栏，兼容存在虚拟键设备因导航透明后按键不易查看的问题 添加header组件

---
+++ app/src/main/java/com/example/snowson/apptest/activity/TransparentStatusActivity.java
@@ -0,0 +1,24 @@
+package com.example.snowson.apptest.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.snowson.apptest.R;
+import com.example.snowson.apptest.utils.ScreenUtils;
+
+public class TransparentStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ScreenUtils.transparentStatus(this);
+        setContentView(R.layout.activity_trans_tarent_status);
+
+        initHeader();
+    }
+
+    private void initHeader() {
+        findViewById(R.id.tv_header_search).setVisibility(View.GONE);
+    }
+}
--- app/src/main/java/com/example/snowson/apptest/utils/ScreenUtils.java
+++ app/src/main/java/com/example/snowson/apptest/utils/ScreenUtils.java
@@ -1,11 +1,16 @@
 package com.example.snowson.apptest.utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.TextUtils;
 import android.text.style.AbsoluteSizeSpan;
 import android.util.TypedValue;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 /**
@@ -53,5 +58,18 @@ public class ScreenUtils {
             return;
         }
         Toast.makeText(context, content, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void transparentStatus(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
     }
 }
