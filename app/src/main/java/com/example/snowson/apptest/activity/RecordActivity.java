f5f67bed54a2706477f4ff502b344c5d1fa93d19
Snowson <snowsonz@163.com>
2018-01-11 18:26:29 +0800

    添加录音界面及音频编解码so库

--- app/src/main/java/com/example/snowson/apptest/activity/RecordActivity.java
+++ app/src/main/java/com/example/snowson/apptest/activity/RecordActivity.java
@@ -1,12 +1,15 @@
 package com.example.snowson.apptest.activity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.snowson.apptest.R;
 
 public class RecordActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
