f2aabc58ab52c4d45bb403cbcb7827f087a94db1
Snowson <snowsonz@163.com>
2018-01-22 16:41:14 +0800

    Rx publish connect , lameda使用

--- app/src/main/java/com/example/snowson/apptest/activity/RxTestActivity.java
+++ app/src/main/java/com/example/snowson/apptest/activity/RxTestActivity.java
@@ -2,23 +2,29 @@ package com.example.snowson.apptest.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.snowson.apptest.R;
 import com.example.snowson.apptest.bean.rx.BaseUserInfo;
 import com.example.snowson.apptest.bean.rx.ExtraUserInfo;
-import com.example.snowson.apptest.bean.rx.UserInfo;
-import com.example.snowson.apptest.net.RetrofitBuilder;
 import com.example.snowson.apptest.bean.rx.LoginRequest;
 import com.example.snowson.apptest.bean.rx.LoginResponse;
 import com.example.snowson.apptest.bean.rx.RegisterRequest;
 import com.example.snowson.apptest.bean.rx.RegisterResponse;
+import com.example.snowson.apptest.bean.rx.UserInfo;
+import com.example.snowson.apptest.net.RetrofitBuilder;
 import com.example.snowson.apptest.net.retrofittest.RetrofitTest;
 import com.example.snowson.apptest.utils.ScreenUtils;
+import com.jakewharton.rxbinding2.view.RxView;
+import com.jakewharton.rxbinding2.widget.RxTextView;
 import com.orhanobut.logger.Logger;
 
 import org.reactivestreams.Subscription;
+
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
@@ -36,6 +42,7 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.observables.ConnectableObservable;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 
@@ -48,6 +55,10 @@ public class RxTestActivity extends AppCompatActivity {
     private Disposable mDisposable;
     private CompositeDisposable mDisposables = new CompositeDisposable();
     private Subscription mSubscription;
+    private EditText mTestEt;
+    private TextView mResultTv;
+    private Button mUnsubscribeBtn;
+    private CompositeDisposable mCompositeDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,8 +66,15 @@ public class RxTestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rx_test);
         initView();
         initEvent();
-        testSyncObserable("one", "two", "three", "four", "five");
-        testAsyncObserable();
+//        testSyncObserable("one", "two", "three", "four", "five");
+//        testAsyncObserable();
+
+        Observable.create(new ObservableOnSubscribe<Void>() {
+            @Override
+            public void subscribe(ObservableEmitter<Void> emitter) throws Exception {
+
+            }
+        });
     }
 
     private void backpressObservable() {
@@ -253,18 +271,58 @@ public class RxTestActivity extends AppCompatActivity {
     }
 
 
+    //publish connect   mutilpile subscribers
     private void initEvent() {
-        mRxTestBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        ConnectableObservable<String> obs = RxTextView.textChangeEvents(mTestEt)
+                .map(event -> event.text().toString()).publish();
+        obs.subscribe(content -> mResultTv.setText(content));
+        obs.subscribe(content -> ScreenUtils.showToast(this, content));
+        mCompositeDisposable = new CompositeDisposable();
+        Disposable connect = obs.connect();
+        mCompositeDisposable.add(connect);
 
+        RxView.clicks(mUnsubscribeBtn)
+                .throttleLast(500, TimeUnit.MICROSECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(Object obj) throws Exception {
+                        //取消注册
+                        mCompositeDisposable.dispose();
+                    }
+                });
+
+        ConnectableObservable<Integer> obsI = Observable.range(7, 1000)
+                .sample(2, TimeUnit.MICROSECONDS).publish();
+        Disposable disposable3 = obsI.subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.i(TAG, " ##1--> " + integer);
             }
         });
+
+        Disposable disposable4 = obsI.subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.i(TAG, " ##2--> " + integer);
+            }
+        });
+        Disposable disposable5 = obsI.subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.i(TAG, " ##3--> " + integer);
+            }
+        });
+        mCompositeDisposable.add(disposable3);
+        mCompositeDisposable.add(disposable4);
+        mCompositeDisposable.add(disposable5);
+        obsI.connect();
     }
 
     private void initView() {
         mRxTestBtn = findViewById(R.id.btn_test);
-        Logger.d(TAG + " > " + "initView method");
+        mTestEt = findViewById(R.id.et_input_test);
+        mResultTv = findViewById(R.id.tv_result);
+        mUnsubscribeBtn = findViewById(R.id.btn_unsubscribe);
     }
 
     @Override
